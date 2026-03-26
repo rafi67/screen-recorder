dfa07007620da6ab2a6de7db87b83b7a31cc5d25
Rafi Siddique <rafisiddique67@gmail.com>
2026-03-26 17:48:39 +0600

    [Done] fix import issue

--- src/app/video/[id]/page.tsx
+++ src/app/video/[id]/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import { getAssetStatus } from "@/app/actions";
-import MuxPlayerWrapper from "@/components/MuxPlayerWrapper";
-import VideoStatusPoller from "@/components/VideoStatusPoller";
-import ShareButton from "@/components/ShareButton";
-import VideoSummary from "@/components/VideoSummary";
+import MuxPlayerWrapper from "../../../../components/MuxPlayerWrapper";
+import VideoStatusPoller from "../../../../components/VideoStatusPoller";
+import ShareButton from "../../../../components/ShareButton";
+import VideoSummary from "../../../../components/VideoSummary";
 import { ArrowLeft, Download } from "lucide-react";
 
 export default async function VideoPage({
