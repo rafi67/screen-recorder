60cb0ba843452e47da6b43ea93d07efd83ac276f
Rafi Siddique <rafisiddique67@gmail.com>
2026-03-26 12:54:38 +0600

    [Done] create mux player wrapper component

---
+++ components/MuxPlayerWrapper.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import MuxPlayer from "@mux/mux-player-react";
+
+type MuxPlayerWrapperProps = {
+  playbackId: string;
+  title?: string;
+};
+
+const MuxPlayerWrapper = ({ playbackId, title }: MuxPlayerWrapperProps) => {
+  return <MuxPlayer 
+    playbackId={playbackId}
+    metadata={{
+        video_title: title || 'Screen Recording',
+    }}
+    streamType="on-demand"
+    autoPlay={false}
+    accentColor="#3b82f6"
+  />;
+};
+
+export default MuxPlayerWrapper;
